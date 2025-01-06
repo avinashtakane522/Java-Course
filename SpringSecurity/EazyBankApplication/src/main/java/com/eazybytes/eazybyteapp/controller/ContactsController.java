993213c548460460464e6a40216517a488eb6cce
Avinash Takane <takaneavinash@gmail.com>
2025-01-07 01:34:25 +0530

    EazyBytes Bank application

---
+++ SpringSecurity/EazyBankApplication/src/main/java/com/eazybytes/eazybyteapp/controller/ContactsController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazybyteapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactsController {
+
+    @GetMapping("/contact")
+    public String getContact() {
+        return "How may i help u...?";
+    }
+}
