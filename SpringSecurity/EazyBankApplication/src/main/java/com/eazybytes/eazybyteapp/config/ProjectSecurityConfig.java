83c7dd3218e0e56fce9b45c658bb8b8f5e13aac6
Avinash Takane <takaneavinash@gmail.com>
2025-01-08 23:43:18 +0530

    EazyBytes Bank application

--- SpringSecurity/EazyBankApplication/src/main/java/com/eazybytes/eazybyteapp/config/ProjectSecurityConfig.java
+++ SpringSecurity/EazyBankApplication/src/main/java/com/eazybytes/eazybyteapp/config/ProjectSecurityConfig.java
@@ -2,18 +2,21 @@ package com.eazybytes.eazybyteapp.config;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers("myAccount", "myBalance", "myLoans", "myCards").authenticated().
-                requestMatchers("notices", "contact").permitAll());
+        http.authorizeHttpRequests((requests) -> requests.
+                requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated().
+                requestMatchers("/notices","/contact","/error").permitAll());
         http.formLogin(flc -> flc.disable());
         http.httpBasic(withDefaults());
         return http.build();
