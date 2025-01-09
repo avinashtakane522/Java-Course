f36e334fc57337e5cc9df16cb703f3b9900e37d5
Avinash Takane <takaneavinash@gmail.com>
2025-01-09 14:55:52 +0530

    Spring Security Basics and Default Configurations

---
+++ SpringSecurity/CustomConfigurationInSWS/src/main/java/com/eazybytes/CustomConfigurationInSWS/CustomConfigurationInSwsApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.CustomConfigurationInSWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomConfigurationInSwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomConfigurationInSwsApplication.class, args);
+	}
+
+}
---
+++ SpringSecurity/CustomConfigurationInSWS/src/test/java/com/eazybytes/CustomConfigurationInSWS/CustomConfigurationInSwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.CustomConfigurationInSWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomConfigurationInSwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
